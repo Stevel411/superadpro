d9c7d1822f80974c8134dcca7bff8b8507dd9f45
SuperAdPro Dev <dev@superadpro.com>
2026-04-07 14:24:53 +0000

    Rebuild static/app/ with SuperDeck layering + styled dropdowns

--- static/app/assets/IncomeGrid3D-DTJo_4a1.js
+++ static/app/assets/IncomeGrid3D-DTJo_4a1.js
@@ -1,4 +1,4 @@
-import{r as Le,ag as q_,j as ze}from"./index-CJW4wdV8.js";/**
+import{r as Le,ag as q_,j as ze}from"./index-BVeOttRF.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
