e747e4c7da9db14e824223b63d6fc34c774dc66b
SuperAdPro Dev <dev@superadpro.com>
2026-04-11 15:51:43 +0000

    i18n: Upgrade fully translated ALL 20 languages (48 keys × 20 = 960 translations)

--- static/app/assets/type-0U0Gy_rM.js
+++ static/app/assets/type-0U0Gy_rM.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-6frXGYMD.js";/**
+import{c as e}from"./index-DBBzPl92.js";/**
  * @license lucide-react v0.454.0 - ISC
  *
  * This source code is licensed under the ISC license.
