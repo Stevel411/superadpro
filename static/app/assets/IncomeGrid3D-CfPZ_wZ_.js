bc18e852ba0ae04fe52e3989e2b1ac429cf2c800
SuperAdPro Dev <dev@superadpro.com>
2026-04-12 10:09:25 +0000

    i18n: AR + HI fully translated ALL remaining namespaces. 12/20 languages COMPLETE. 8 remaining: NL/PL/TR/VI/TH/ID/TL/SW

--- static/app/assets/IncomeGrid3D-CfPZ_wZ_.js
+++ static/app/assets/IncomeGrid3D-CfPZ_wZ_.js
@@ -1,4 +1,4 @@
-import{r as Le,ad as q_,j as ze}from"./index-vzbxcFUY.js";/**
+import{r as Le,ad as q_,j as ze}from"./index-BJ0zGvFs.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
