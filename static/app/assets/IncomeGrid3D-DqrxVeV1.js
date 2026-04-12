ac64cb8d63adaadbf0fc5f905eb45d3905628442
SuperAdPro Dev <dev@superadpro.com>
2026-04-12 08:46:28 +0000

    i18n: Auth (70 keys) + ContentCreator (66 keys) now COMPLETE in ALL 20 languages. 2,720 new translations. 23 namespaces remain

--- static/app/assets/IncomeGrid3D-DqrxVeV1.js
+++ static/app/assets/IncomeGrid3D-DqrxVeV1.js
@@ -1,4 +1,4 @@
-import{r as Le,ad as q_,j as ze}from"./index-CtnGT4yi.js";/**
+import{r as Le,ad as q_,j as ze}from"./index-Co4vq-ea.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
